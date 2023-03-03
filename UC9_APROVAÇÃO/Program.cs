ec9f1093b74e85ccbf56eedd78b54d609f4b0e0e
MARINA QUEIROZ RIBEIRO DE PAULA <marina.qrpaula@senacsp.edu.br>
2023-03-02 21:56:34 -0300

    aprovação

--- UC9_APROVAÇÃO/Program.cs
+++ UC9_APROVAÇÃO/Program.cs
@@ -1,15 +1,22 @@
 ﻿string[,] cadastro = new string[5,7];
 int i = 0;
-string nome = "";
+string nomes = "";
+int linha = 0;
+int coluna = 0;
 
 for(i = 0; i <= 4; i++)
 {
-    Console.WriteLine("Cadastre o nome do aluno(a)");
-    nome = Console.ReadLine();
+   
+        Console.WriteLine("Cadastre o nome do aluno(a)");
+        nomes = Console.ReadLine();
+  
 }
+
 for (i = 0;i <= 4; i++)
 {
-    Console.WriteLine(nome + " - ");
+   
+        Console.WriteLine(nomes + "");
+    
 }
 
 
